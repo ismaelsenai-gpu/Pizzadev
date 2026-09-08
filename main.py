503a84a398c4c5346e361a8a064d7e1e22963ef4
Ismael <ismael.o.lima6@aluno.senai.br>
2026-09-08 09:46:26 -0300

    Criação de laço de escolha de pizza

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
-pizzas = ["Calabresa", "Frango com Catupiry", "Portuguesa", "Marguerita"]
+pizzas = ["Calabresa", "Frango com Catupiry", "Portuguesa", "Marguerita", "Chocolate"]
 
-precos = [30.0, 35.0, 40.0, 30.0]
+precos = [30.0, 35.0, 40.0, 30.0, 45.0]
 
 print("-- Cardápio de Pizzas --")
 for i in range(len(pizzas)):
     print(f"{i + 1}. {pizzas[i]} - R$ {precos[i]:.2f}")
 
+while True:
+    escolha = input("Escolha o numero da pizza que deseja pedir (ou 'sair' para encerrar): ")
+    if escolha.lower() == 'sair':
+        print("Obrigado por visitar nosso cardapio! VOLTE SEMPREEE")
+        break
+    elif escolha.isdigit() and 1 <= int (escolha) <= len(pizzas):
+        indice = int(escolha) - 1
+        print(f"Voce escolheu a pizza {pizzas[indice]} que custa R$ {precos[indice]:.2f}")
 
+"""cat << 'EOF'
+███████╗ █████╗ ███╗   ███╗██╗   ██╗███████╗██╗     
+██╔════╝██╔══██╗████╗ ████║██║   ██║██╔════╝██║     
+███████╗███████║██╔████╔██║██║   ██║█████╗  ██║     
+╚════██║██╔══██║██║╚██╔╝██║██║   ██║██╔══╝  ██║     
+███████║██║  ██║██║ ╚═╝ ██║╚██████╔╝███████╗███████╗
+╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚══════╝╚══════╝
+
+███████╗    ███████╗    ██╗         █████╗     ███████╗
+██╔════╝    ██╔════╝    ██║        ██╔══██╗    ██╔════╝
+█████╗      █████╗      ██║        ███████║    █████╗  
+██╔══╝      ██╔══╝      ██║        ██╔══██║    ██╔══╝  
+███████╗    ███████╗    ███████╗   ██║  ██║    ███████╗
+╚══════╝    ╚══════╝    ╚══════╝   ╚═╝  ╚═╝    ╚══════╝
+
+              SAMUEL É LEGAL DE MAIS! 😎🔥
+EOF"""
