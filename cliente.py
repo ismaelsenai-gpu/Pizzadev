90f8f2ad65660f33baad480cde09120dc49138ec
Ismael <ismael.o.lima6@aluno.senai.br>
2026-09-09 09:49:08 -0300

    Modificação, cadastro, validação, mostrar clientes

--- cliente.py
+++ cliente.py
@@ -1,33 +1,50 @@
 clientes = []
 
 def cadastrar_clientes():
-    nome = input("Digite o nome do cliente: ").strip()
+    while True:
+        print("\n-- Cadastro de Clientes --")
+        escolha = input("Deseja cadastrar um novo cliente? (s/n): ").strip().lower()
+        if escolha == 'n':
+            break
+        nome = input("Digite o nome do cliente: ").strip()
 
-    if nome == "":
-        print("Nome não pode ser vazio. Por favor, digite um nome válido.")
-        return
+        if nome == "":
+            print("Nome não pode ser vazio. Por favor, digite um nome válido.")
+            return
+        try:
+            int(nome)
+            print("Nome nao pode ser um numero. Por favor, digite um nome valido")
+        except ValueError:
+            pass
+        telefone = input("Digite o telefone do cliente: ").strip()
 
-    telefone = input("Digite o telefone do cliente: ").strip()
+        if telefone == "":
+            print("Telefone não pode ser vazio. Por favor, digite um telefone válido.")
+            return
 
-    if telefone == "":
-        print("Telefone não pode ser vazio. Por favor, digite um telefone válido.")
-        return
+        try:
+            telefone = int(telefone)
+        except ValueError:
+            print("Telefone deve ser um número inteiro. Digite um número válido.")
+            return
 
-    try:
-        telefone = int(telefone)
-    except ValueError:
-        print("Telefone deve ser um número inteiro. Digite um número válido.")
-        return
+        cliente = {
+            "nome": nome,
+            "telefone": telefone
+        }
 
-    cliente = {
-        "nome": nome,
-        "telefone": telefone
-    }
-
-    if cliente not in clientes:
-        clientes.append(cliente)
-        print("Cliente cadastrado com sucesso!")
+        if cliente not in clientes:
+            clientes.append(cliente)
+            print("Cliente cadastrado com sucesso!")
+        else:
+            print("Cliente já cadastrado.")
+def mostrar_clientes():
+    if not clientes:
+        print("Nenhum cliente cadastrado.")
     else:
-        print("Cliente já cadastrado.")
+        print("-- LISTA DE CLIENTES --")
+        for i, cliente in enumerate(clientes):
+            print(f"{i + 1}. Nome: {cliente['nome']}, telefone: {cliente['telefone']}")
 
 cadastrar_clientes()
+mostrar_clientes()
