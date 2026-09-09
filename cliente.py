23e5f8d0bfb20602edf28b36078675f2c98c9c00
Ismael <ismael.o.lima6@aluno.senai.br>
2026-09-09 08:52:53 -0300

    Validações

--- cliente.py
+++ cliente.py
@@ -1,11 +1,33 @@
 clientes = []
 
 def cadastrar_clientes():
-    cliente  = {
-        "nome": input("Digite o nome do cliente: "),
-        "telefone": input(f"Digite o telefone do cliente: ")
+    nome = input("Digite o nome do cliente: ").strip()
+
+    if nome == "":
+        print("Nome não pode ser vazio. Por favor, digite um nome válido.")
+        return
+
+    telefone = input("Digite o telefone do cliente: ").strip()
+
+    if telefone == "":
+        print("Telefone não pode ser vazio. Por favor, digite um telefone válido.")
+        return
+
+    try:
+        telefone = int(telefone)
+    except ValueError:
+        print("Telefone deve ser um número inteiro. Digite um número válido.")
+        return
+
+    cliente = {
+        "nome": nome,
+        "telefone": telefone
     }
 
-    clientes.append(cliente)
+    if cliente not in clientes:
+        clientes.append(cliente)
+        print("Cliente cadastrado com sucesso!")
+    else:
+        print("Cliente já cadastrado.")
 
 cadastrar_clientes()
